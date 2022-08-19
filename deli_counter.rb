b786f26d78df9a050f591e8e24da2fcf009b72c4
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-19 14:06:56 +0300

    commit tree f2113d36f8a1078007993289f39f51e98ed6caab

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def line (array)
+    if array.length == 0
+        puts "The line is currently empty."
+    else
+        name = ''
+        array.each_with_index do |person, index|
+            name += " #{index + 1}. #{person}"
+        end
+        puts "The line is currently:" << "#{name}"
+        
+    end
+end
+
+def take_a_number (array, person)
+    array.push(person)
+    position = array.length
+    puts "Welcome, #{person}. You are number #{position} in line."
+end
+
+def now_serving (array)
+    
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
+
+line (["Logan", "Avi", "Spencer"])
